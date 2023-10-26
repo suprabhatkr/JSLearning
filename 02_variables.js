fa16530e0ebfcd6d0a1b04639352afd04dbd7f3b
Suprabhat kumar <suprabhat@Suprabhats-MacBook-Air.local>
2023-10-26 14:17:26 +0530

    Variables added in Javascript

---
+++ 02_variables.js
@@ -0,0 +1,11 @@
+// Number type (integer)
+var a = Number(9);
+console.log(`Type of variable ${a} is ${typeof(a)}`);
+
+// Number type (float)
+var b = 1.2;
+console.log(`Type of variable ${b} is ${typeof(b)}`);
+
+// char type
+var c = 'a';
+console.log(`Type of ${c} is ${typeof(c)}`);
